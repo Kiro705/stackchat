bb76ad7e87ea2164414411a8672ef3d1f4e819ef
David Eiber <davideiber@gmail.com>
2017-08-23 17:52:27 -0400

    Finish 'The Store'

---
+++ client/store.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux';
+
+const initialState = {
+    messages: []
+};
+
+function reducer (state = initialState, action) {
+    switch (action.type) {
+      case GOT_MESSAGES_FROM_SERVER: 
+         return Object.assign({}, state, { messages: action.messages });
+      default: 
+         return state;
+    }
+  }
+  
+
+const GOT_MESSAGES_FROM_SERVER = 'GOT_MESSAGES_FROM_SERVER';
+
+const store = createStore(reducer)
+export default store;
+
+export const gotMessagesFromServer = function(messageArray){
+    return {
+        type: GOT_MESSAGES_FROM_SERVER,
+        messages: messageArray
+    };
+};
